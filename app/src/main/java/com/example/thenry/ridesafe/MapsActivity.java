e50a5ad07cae16e28b41a74e9e6d2518f7fcf237
ThomaHe <githubpass1>
2017-02-10 10:23:24 +0100

    ajout de la barre de recherche avec fonction de recherche

--- app/src/main/java/com/example/thenry/ridesafe/MapsActivity.java
+++ app/src/main/java/com/example/thenry/ridesafe/MapsActivity.java
@@ -2,6 +2,8 @@ package com.example.thenry.ridesafe;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,9 +14,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +36,9 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -56,6 +64,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     TextView desc_sheet;
     @BindView(R.id.add_fab)
     FloatingActionButton add_fab;
+    @BindView(R.id.address_search)
+    EditText address_search;
 
 
     // variables pour la mise à jour de la localisation
@@ -105,6 +115,42 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onSlide(@NonNull View bottomSheet, float slideOffset) {
 
+            }
+        });
+
+        address_search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEARCH) {
+                    String location = address_search.getText().toString();
+                    List<Address> addressList = null;
+
+                    if (location != null || !location.equals("")) {
+                        Geocoder geocoder = new Geocoder(MapsActivity.this);
+                        try {
+                            addressList = geocoder.getFromLocationName(location, 1);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if(addressList.size()!=0) {
+                            Address address = addressList.get(0);
+                            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                            mMap.addMarker(new MarkerOptions()
+                                    .position(latLng)
+                                    .title("Nouveau")
+                                    .draggable(true)
+                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Adresse inconnue", Toast.LENGTH_LONG).show();
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                return false;
             }
         });
 
